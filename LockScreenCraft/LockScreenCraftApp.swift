a7d17ec7cc557ae50a70c0c0f34920dae29c3a08
alexlin519 <alex273988@gmail.com>
2025-01-25 01:35:54 -0800

    Initial Commit

---
+++ LockScreenCraft/LockScreenCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenCraftApp.swift
+//  LockScreenCraft
+//
+//  Created by Alex Lin on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LockScreenCraftTests/LockScreenCraftTests.swift
@@ -0,0 +1,16 @@
+//
+//  LockScreenCraftTests.swift
+//  LockScreenCraftTests
+//
+//  Created by Alex Lin on 2025-01-25.
+//
+
+import Testing
+
+struct LockScreenCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
